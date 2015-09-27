12af20d5e1508dae317eb946e1eec4fe1da9a771
Mehrtash Babadi <mehrtashbabadi@Mehrtashs-MacBook-Pro.local>
2015-09-26 23:23:32 -0700

    Initial Commit

---
+++ MigdalEliashbergSolver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MigdalEliashbergSolver
+//
+//  Created by Mehrtash Babadi on 9/26/15.
+//  Copyright (c) 2015 mbabadi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
